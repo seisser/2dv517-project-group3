f41460fa3d929d0b9bd967c0d4051349a70a4b62
linusmortzell <lm222qm@student.lnu.se>
2020-10-21 16:52:29 +0200

    Bugfix and adding database acmedb

--- Scripts/dbmaster.sh
+++ Scripts/dbmaster.sh
@@ -7,9 +7,10 @@
 slaveip=$1
 
 sudo mysql --user=root <<_EOF_
-        CREATE USER 'acme_user'@'$slaveip' IDENTIFIED BY 'acme_password';
+        CREATE USER 'acme_user'@'$slaveip' IDENTIFIED BY '';
         GRANT REPLICATION SLAVE ON *.* TO 'acme_user'@'$slaveip';
-        ALTER USER acme_user$slaveip IDENTIFIED WITH mysql_native_password BY 'acme_password';
+        ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root_password';
+        CREATE DATABASE acmedb;
 _EOF_
 
 sudo mysql -u root -e "SHOW MASTER STATUS\G" | grep 'File\|Position' | sed 's/^ *//' > masterstatus.yml
