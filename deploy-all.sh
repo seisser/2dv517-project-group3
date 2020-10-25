e537a22d4224c9c2dc1fdbd4552abfdc7d3886e1
Daniela Rondahl <dr222ew@student.lnu.se>
2020-10-25 22:23:29 +0100

    Fixed path to internal-software-up

--- deploy-all.sh
+++ deploy-all.sh
@@ -6,7 +6,7 @@ ansible-playbook Playbooks/external-playbooks/external-all-up.yml
 ssh -tt -i ~/.ssh/acme-key.pem ubuntu@194.47.176.245 << EOF
 
 ansible-playbook Playbooks/internal-playbooks/all-internal-infra-up.yml
-ansible-playbook Playbooks/all-internal-software-up.yml
+ansible-playbook Playbooks/internal-playbooks/all-internal-software-up.yml
 
 exit
 EOF
