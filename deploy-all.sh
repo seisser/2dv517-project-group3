9064c5c5e8cd79971c9857e5ecf94a98b775e2aa
Daniela Rondahl <dr222ew@student.lnu.se>
2020-10-25 21:50:48 +0100

    moved external-all-up and fixed path

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 tar -cvf project.tar.gz *
-ansible-playbook Playbooks/external-all-up.yml
+ansible-playbook Playbooks/external-playbooks/external-all-up.yml
 
 ssh -tt -i ~/.ssh/acme-key.pem ubuntu@194.47.176.245 << EOF
 
